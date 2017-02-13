4e0ee3dc6e61cf1ec67158bd8ddec3b3894119c1
Дмитрий Карасев <dskarasev@bars-open.ru>
2017-02-13 14:43:48 +0300

    # 13_02_2017 14:45 Добавлен Ioc контейнер Niject (пока только в проект для парсинга XML)

---
+++ 1CIntegrationParserXML/ParserXmlFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Ninject.Modules;
+
+namespace _1CIntegrationParserXML
+{
+    public class ParserXmlFactory : NinjectModule
+    {
+        private void FindTemplate(string fullPathFile, string nameFile)
+        {
+            try
+            {
+                var parsingImpl = Bind<IBaseParserXml>().To<IBaseParserXml>().WithPropertyValue("NameFile", nameFile.Replace(".xml", ""));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void RunParser()
+        {
+            
+        }
+
+        public override void Load()
+        {
+            
+        }
+    }
+
+    public interface IBaseParserXml
+    {
+        string NameFile { get; }
+    }
+
+    public class ParserXmlNameImport : IBaseParserXml
+    {
+        public string NameFile
+        {
+            get { return "import"; }
+        }
+    }
+}
