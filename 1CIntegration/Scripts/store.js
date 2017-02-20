73a500009ef01e86618c840b8c3cce7c7405f63e
LAPTOP-FGT30DG7\Дмитрий <KarasevDmitiyS@yandex.ru>
2017-02-20 11:42:11 +0300

    #20_02_2017 11:40 Пример создания div + store.js

---
+++ 1CIntegration/Scripts/store.js
@@ -0,0 +1,32 @@
+﻿
+var Djinaro = {};
+
+Djinaro.WriteResponse = function (data) {
+    var categories = document.getElementById('categories');
+    data.forEach(function (item, i, arr) {
+        debugger;
+        var row = document.createElement('div');
+        row.className = 'row';
+
+        var div = document.createElement('div');
+        div.className = 'col-md-12';
+        div.innerHTML = data[0].good;
+
+        row.appendChild(div);
+        categories.appendChild(row);
+    });
+}
+
+Djinaro.getGoods = function () {
+    $.ajax({
+        url: '/api/values',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            Djinaro.WriteResponse(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
