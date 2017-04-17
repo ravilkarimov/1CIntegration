ed5f53c79af16519f3d8a9cbab55a58dfe575483
Ravil Karimov <r.karimov@bars-open.ru>
2017-04-17 10:43:08 +0300

    #17_04_2017 Проверки перед создание sysobjects в db

--- 1CIntegrationDB/DBProgram.cs
+++ 1CIntegrationDB/DBProgram.cs
@@ -12,71 +12,76 @@ namespace _1CIntegrationDB
 
         void createTables()
         {
-            const string sql_brands = "CREATE TABLE d_brands (" +
+            const string sql_brands = "IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME='d_brands' AND xtype='U') " +
+                                      "CREATE TABLE d_brands (" +
                                       "brand_id INT IDENTITY(1,1) NOT NULL, " +
                                       "brand nvarchar(255), " +
                                       "is_actual INT) ";
 
-            const string sql_groups = "CREATE TABLE groups (" +
-                                "group_id INT IDENTITY(1,1) NOT NULL, " +
-                                "group_key nvarchar(255), " +
-                                "group_name nvarchar(255), " +
-                                "is_actual INT) ";
+            const string sql_groups = "IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME='groups' AND xtype='U') " +
+                                      "CREATE TABLE groups (" +
+                                      "group_id INT IDENTITY(1,1) NOT NULL, " +
+                                      "group_key nvarchar(255), " +
+                                      "group_name nvarchar(255), " +
+                                      "is_actual INT) ";
 
-            const string sql_goods = "CREATE TABLE goods (" +
-                               "good_id INT IDENTITY(1,1) NOT NULL, " +
-                               "good_key nvarchar(255), " +
-                               "good nvarchar(255), " +
-                               "group_id INT, " +
-                               "brand_id INT, " +
-                               "inserted_on date, " +
-                               "changed_on date, " +
-                               "img_path nvarchar(500), " +
-                               "is_actual INT) ";
+            const string sql_goods = "IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME='goods' AND xtype='U') " +
+                                     "CREATE TABLE goods (" +
+                                     "good_id INT IDENTITY(1,1) NOT NULL, " +
+                                     "good_key nvarchar(255), " +
+                                     "good nvarchar(255), " +
+                                     "group_id INT, " +
+                                     "brand_id INT, " +
+                                     "inserted_on DATE, " +
+                                     "changed_on DATE, " +
+                                     "img_path nvarchar(500), " +
+                                     "is_actual INT) ";
 
-            const string sql_offers = "CREATE TABLE offers (" +
-                                "offer_id INT IDENTITY(1,1) NOT NULL, " +
-                                "offer_key nvarchar(255), " +
-                                "good_key nvarchar(255), " +
-                                "feature nvarchar(255), " +
-                                "size nvarchar(20), " +
-                                "price INT, " +
-                                "currency nvarchar(255), " +
-                                "amount INT, " +
-                                "inserted_on date, " +
-                                "changed_on date ) ";
+            const string sql_offers = "IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME='offers' AND xtype='U') " +
+                                      "CREATE TABLE offers (" +
+                                      "offer_id INT IDENTITY(1,1) NOT NULL, " +
+                                      "offer_key nvarchar(255), " +
+                                      "good_key nvarchar(255), " +
+                                      "feature nvarchar(255), " +
+                                      "size nvarchar(20), " +
+                                      "price INT, " +
+                                      "currency nvarchar(255), " +
+                                      "amount INT, " +
+                                      "inserted_on DATE, " +
+                                      "changed_on DATE) ";
 
-            const string sql_d_features = "CREATE TABLE features (" +
-                                    "feature_id INT IDENTITY(1,1) NOT NULL, " +
-                                    "good_id INT, " +
-                                    "feature nvarchar(255) " +
-                                    "value INT " +
-                                    "feature_key nvarchar(255) ) ";
+            const string sql_d_features = "IF NOT EXISTS (SELECT * FROM SYSOBJECTS WHERE NAME='features' AND xtype='U') " +
+                                          "CREATE TABLE features (" +
+                                          "feature_id INT IDENTITY(1,1) NOT NULL, " +
+                                          "good_id INT, " +
+                                          "feature nvarchar(255), " +
+                                          "value INT, " +
+                                          "feature_key nvarchar(255) ) ";
 
             SQLiteProvider.DoSql(sql_brands);
             SQLiteProvider.DoSql(sql_groups);
             SQLiteProvider.DoSql(sql_goods);
-            SQLiteProvider.DoSql("CREATE UNIQUE INDEX ix_goods_good_id ON goods (good_id)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_goods_good_id') CREATE UNIQUE INDEX ix_goods_good_id ON goods (good_id)");
             SQLiteProvider.DoSql(sql_offers);
             SQLiteProvider.DoSql(sql_d_features);
 
-            SQLiteProvider.DoSql("create index ix_offers_price On  offers(price)");
-            SQLiteProvider.DoSql("create index ix_offers_amount On  offers(amount)"); 
-            SQLiteProvider.DoSql("create index ix_offers_size On  offers(size)");
-            SQLiteProvider.DoSql("create index ix_goods_group_id On  goods(group_id)");
-            SQLiteProvider.DoSql("create index ix_goods_brand_id On  goods(brand_id)");
-            SQLiteProvider.DoSql("create index ix_goods_good On  goods(good)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_offers_price') create index ix_offers_price On  offers(price)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_offers_amount') create index ix_offers_amount On  offers(amount)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_offers_size') create index ix_offers_size On  offers(size)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_goods_group_id') create index ix_goods_group_id On  goods(group_id)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_goods_brand_id') create index ix_goods_brand_id On  goods(brand_id)");
+            SQLiteProvider.DoSql("IF NOT EXISTS (SELECT * FROM sysindexes WHERE name='ix_goods_good') create index ix_goods_good On  goods(good)");
 
             const string sqlTriggerGoodsInsert = "create trigger trigger_goods_insert " +
-                                                    "on goods " +
-                                                    "for insert " +
-                                                    "AS " +
-                                                    "if @@ROWCOUNT > 0 " +
-                                                    "begin " +
-                                                    "   update goods " +
-                                                    "   set inserted_on = GETDATE() " +
-                                                    "   where good_id in (select good_id from INSERTED) " +
-                                                    "end";
+                                                 "on goods " +
+                                                 "for insert " +
+                                                 "AS " +
+                                                 "if @@ROWCOUNT > 0 " +
+                                                 "begin " +
+                                                 "   update goods " +
+                                                 "   set inserted_on = GETDATE() " +
+                                                 "   where good_id in (select good_id from INSERTED) " +
+                                                 "end";
 
             const string sqlTriggerGoodsUpdate = "create trigger trigger_goods_update " +
                                                     "on goods " +
