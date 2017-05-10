125548ca9aba46505d78a7fda52c523d3e0378ce
Ravil Karimov <r.karimov@bars-open.ru>
2017-05-10 10:18:14 +0300

    #10_05_2017 Сайдбар меню

---
+++ 1CIntegration/Scripts/swipemenu.js
@@ -0,0 +1,21 @@
+﻿jQuery(document).ready(function () {
+    jQuery("[data-toggle]").click(function () {
+        var sidebar = jQuery("#sidebar");
+        if (sidebar.hasClass('open-sidebar')) {
+            sidebar.removeClass("open-sidebar");
+        } else sidebar.addClass("open-sidebar");
+    });
+
+});
+jQuery(".swipe-area").swipe({
+    swipeStatus: function (event, phase, direction, distance, duration, fingers) {
+        if (phase == "move" && direction == "right") {
+            jQuery(".sidebar").addClass("open-sidebar");
+            return false;
+        }
+        if (phase == "move" && direction == "left") {
+            jQuery(".sidebar").removeClass("open-sidebar");
+            return false;
+        }
+    }
+});
