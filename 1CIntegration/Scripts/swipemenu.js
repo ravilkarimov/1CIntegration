7e3fb6835133bf9259ded88567cb1d969ce29075
Ravil Karimov <r.karimov@bars-open.ru>
2017-05-23 22:04:38 +0300

    #23_05_2017 Приближение по мобильной версии

--- 1CIntegration/Scripts/swipemenu.js
+++ 1CIntegration/Scripts/swipemenu.js
@@ -1,20 +1,19 @@
 ﻿jQuery(document).ready(function () {
     jQuery("[data-toggle]").click(function () {
-        var sidebar = jQuery("#sidebar");
-        if (sidebar.hasClass('open-sidebar')) {
-            sidebar.removeClass("open-sidebar");
-        } else sidebar.addClass("open-sidebar");
+        var toggle_el = jQuery(this).data("toggle");
+        jQuery(toggle_el).toggleClass("open-sidebar");
     });
 
 });
+
 jQuery(".swipe-area").swipe({
     swipeStatus: function (event, phase, direction, distance, duration, fingers) {
         if (phase == "move" && direction == "right") {
-            jQuery(".sidebar").addClass("open-sidebar");
+            jQuery(".container").addClass("open-sidebar");
             return false;
         }
         if (phase == "move" && direction == "left") {
-            jQuery(".sidebar").removeClass("open-sidebar");
+            jQuery(".container").removeClass("open-sidebar");
             return false;
         }
     }
