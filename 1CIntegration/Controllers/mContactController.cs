bfd3bb4a2c3402e127ea11ffdf93ddec67678294
Ravil Karimov <r.karimov@bars-open.ru>
2017-06-27 18:41:48 +0300

    #27_06_16 мобильная вьюха контактов

---
+++ 1CIntegration/Controllers/mContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1CIntegration.Controllers
+{
+    public class mContactController : Controller
+    {
+        //
+        // GET: /mContact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
